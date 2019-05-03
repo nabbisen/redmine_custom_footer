061c521f2da7047c65ad68813db2ffe779c2dd92
heddi.nabbisen <heddi.nabbisen@gmail.com>
2019-05-03 19:31:25 +0900

    implement hook listener

--- init.rb
+++ init.rb
@@ -6,3 +6,12 @@ Redmine::Plugin.register :custom_footer do
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
+
+class CustomFooterHookViewListener < Redmine::Hook::ViewListener
+  def view_layouts_base_html_head(context={})
+    footer_content = 'some_footer_content'
+    [
+      "<script>document.addEventListener('DOMContentLoaded',function(){var footer = document.getElementById('footer').innerHTML='<div class=\    "bgl\"><div class=\"bgr\"><div style=\"text-align:center;\">#{footer_content}</div></div></div>';});</script>",
+    ]
+  end
+end
